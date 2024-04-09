aedbbfd74553bbcd91e2a6a749a5b2ec607e1ad6
NataliaGabriela <natigab.24@gmail.com>
2024-04-09 17:49:07 -0300

    corrección del punto 3

--- tp2_3.c
+++ tp2_3.c
@@ -14,7 +14,7 @@ int main()
         for (j = 0; j < M; j++)
         {
             *(p_mt + (i * M + j)) = 1 + rand() % 100;
-            printf("%d ", (*p_mt)++);
+            printf("%d ", *(p_mt + (i * M + j)));
         }
         printf("\n");
     }
